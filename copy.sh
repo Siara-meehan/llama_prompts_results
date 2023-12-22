f7fcf2ddc8ccf3b29a253ecbfe986077d1f8a6a9
Me <me@example.com>
2023-12-22 07:34:32 -0800

    7b 5ks runs

---
+++ copy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "cp -uv ~/outputs/llama-2-7b-chat.Q5_K_S.gguf/*_done.yaml llama-2-7b-chat.Q5_K_S.gguf"
+cp -uv ~/outputs/llama-2-7b-chat.Q5_K_S.gguf/*_done.yaml llama-2-7b-chat.Q5_K_S.gguf
