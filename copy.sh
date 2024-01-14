06d197fbe10408569d779d34ad7e28dc27de5c1e
Me <me@example.com>
2024-01-14 08:35:00 -0800

    13b and 7b runs

--- copy.sh
+++ copy.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+mkdir -p llama-2-7b-chat.Q5_K_S.gguf
 echo "cp -uv ~/outputs/llama-2-7b-chat.Q5_K_S.gguf/*_done.yaml llama-2-7b-chat.Q5_K_S.gguf"
 cp -uv ~/outputs/llama-2-7b-chat.Q5_K_S.gguf/*_done.yaml llama-2-7b-chat.Q5_K_S.gguf
+
+mkdir -p llama-2-13b-chat.Q5_K_S.gguf
+echo "cp -uv ~/outputs/llama-2-13b-chat.Q5_K_S.gguf/*_done.yaml llama-2-13b-chat.Q5_K_S.gguf"
+cp -uv ~/outputs/llama-2-13b-chat.Q5_K_S.gguf/*_done.yaml llama-2-13b-chat.Q5_K_S.gguf
